720e3d70a3f006d74aba8919c068f494f00ba67f
Noman <nomanmia128@gmail.com>
2024-09-25 10:19:28 +0600

    complit javascript function and DOM add HTML file

--- js/donate.js
+++ js/donate.js
@@ -1,56 +1,164 @@
-// console.log('clicked donate btn');
+// console.log('uitility added');
+function getInputFieldValueById(id){
+    const inputDonate = document.getElementById(id).value;
+    const inputNumber = parseFloat(inputDonate);
+    return inputNumber;
+}
 
+function getTextFieldValueById(id){
+    const textValue = document.getElementById(id).innerText;
+    const textNumber = parseFloat(textValue);
+    return textNumber;
+}
+
+function showScertionById(id){
+    document.getElementById('donate-from').classList.add('hidden');
+    document.getElementById('history-from').classList.add('hidden')
+    document.getElementById(id).classList.remove('hidden');
+}
+
+// section-1 start
 document.getElementById('btn-donate')
 .addEventListener('click', function(event){
    event.preventDefault();
 
-//    donate btn
-   const donet = getInputFieldValueById('input-doneat');
+   const donateInput = getInputFieldValueById('input-doneat');
+   const acountValue = getTextFieldValueById('account-balance');
+   document.getElementById('input-doneat').value = '';
 
-//    main amount
-// const totalAmounnt = getInputFieldValueById('main-amount');
+   if(isNaN(donateInput) || donateInput <= 0 || (donateInput > acountValue)){
+    alert('Invalid Donation Amount')
+    return;
+   }
+   else{
+      document.getElementById('my_modal_1').showModal()
+   }
+ 
+   const balance = getTextFieldValueById('current-amount');
+   const newDonate = balance + donateInput;
+   document.getElementById('current-amount').innerText = newDonate;
 
-   const balance = getTextFieldValueById('section-total-amount');
-   const newDonate = balance + donet;
-
-   const ttttt = getTextFieldValueById('main-amount');
-   const newtttt = ttttt - newDonate;
-
-   document.getElementById('section-total-amount').innerText = newDonate;
-   document.getElementById('main-amount').innerText = newtttt;
-
-//    if(inputValue <=0 || isNaN(inputValue) || (inputValue > balance)){
-//     alert('My code is samthing rong')
-//    }
-
+   const updateBalance = acountValue - donateInput;
+   document.getElementById('account-balance').innerText = updateBalance;
+   
 //    added history
    const div = document.createElement('div');
    div.classList.add('p-5', 'border', 'rounded-xl', 'mb-2');
    div.innerHTML = `
-   <h4 class="text-2xl font-bold pb-2">${newDonate} Taka is Donate for famine-2024 at Feni, Bangladesh</h4>
+   <h4 class="lg:text-2xl font-bold pb-2">${donateInput} Taka is Donate for famine-2024 at Feni, Bangladesh</h4>
    <p>Date: Tue${new Date(). toLocaleString()} GMT +0600 (Bangladesh Standard Time)</p>
    `
    document.getElementById('history-contain').appendChild(div);
 
 })
+// card-1 end
 
-// // section-2
-// ElementById('btn-donate2')
-// .addEventListener('click', function(event){
-//    event.preventDefault();
+// card-2 start
+document.getElementById('btn-donate2')
+.addEventListener('click', function(event2){
+   event2.preventDefault();
 
-// //    donate btn
-//    const donet2 = getInputFieldValueByIdd('input-doneat2');
+   const donateInput2 = getInputFieldValueById('input-doneat2');
+   const acountValue2 = getTextFieldValueById('account-balance');
+   document.getElementById('input-doneat2').value = '';
 
-// //    main amount
-// // const totalAmounnt = getInputFieldValueById('main-amount');
+   if(isNaN(donateInput2) || donateInput2 <= 0 || (donateInput2 > acountValue2)){
+    alert('Invalid Donation Amount')
+    return;
+   }
+   else{
+      document.getElementById('my_modal_1').showModal()
+   }
+ 
+   const balance2 = getTextFieldValueById('current-amount2');
+   const newDonate2 = balance2 + donateInput2;
+   
+   
+   document.getElementById('current-amount2').innerText = newDonate2;
 
-//    const balance2 = getTextFieldValueByIdd('section-total-amount2');
-//    const newDonate2 = balance2 + donet2;
+   const updateBalance2 = acountValue2 - donateInput2;
+   document.getElementById('account-balance').innerText = updateBalance2;
+   
+//    added history
+   const div = document.createElement('div');
+   div.classList.add('p-5', 'border', 'rounded-xl', 'mb-2');
+   div.innerHTML = `
+   <h4 class="text-2xl font-bold pb-2">${donateInput2} Taka is Donated for Flood Relief in Feni,Bangladesh</h4>
+   <p>Date: Tue${new Date(). toLocaleString()} GMT +0600 (Bangladesh Standard Time)</p>
+   `
+   document.getElementById('history-contain').appendChild(div);
 
-// //    const ttttt2 = getTextFieldValueByIdd('main-amount');
-// //    const newtttt2 = ttttt2 - newDonate2;
+})
+// card-2 end
 
-//    document.getElementById('section-total-amount2').innerText = newDonate2;
-// //    document.getElementById('main-amount').innerText = newtttt2;
-// })
+// card-3 start 
+document.getElementById('btn-donate3')
+.addEventListener('click', function(event3){
+   event3.preventDefault();
+
+   const donateInput3 = getInputFieldValueById('input-doneat3');
+   const acountValue3 = getTextFieldValueById('account-balance');
+   document.getElementById('input-doneat3').value = '';
+
+   if(isNaN(donateInput3) || donateInput3 <= 0 || (donateInput3 > acountValue3)){
+    alert('Invalid Donation Amount')
+    return;
+   }
+   else{
+      document.getElementById('my_modal_1').showModal()
+   }
+ 
+   const balance3 = getTextFieldValueById('current-amount3');
+   const newDonate3 = balance3 + donateInput3;
+   
+   
+   document.getElementById('current-amount3').innerText = newDonate3;
+
+   const updateBalance3 = acountValue3 - donateInput3;
+   document.getElementById('account-balance').innerText = updateBalance3;
+   
+//    added history
+   const div = document.createElement('div');
+   div.classList.add('p-5', 'border', 'rounded-xl', 'mb-2');
+   div.innerHTML = `
+   <h4 class="text-2xl font-bold pb-2">${donateInput3} Taka is Donated for Aid for Injured in the Quota Movement, Bangladesh</h4>
+   <p>Date: Tue${new Date(). toLocaleString()} GMT +0600 (Bangladesh Standard Time)</p>
+   `
+   document.getElementById('history-contain').appendChild(div);
+
+})
+// card-3 end
+
+
+
+
+document.getElementById('donate-form-btn')
+.addEventListener('click', function(){
+
+    showScertionById('donate-from');
+})
+
+document.getElementById('history-form-btn')
+.addEventListener('click', function(){
+
+    showScertionById('history-from');
+})
+
+
+const donatttButn = document.getElementById('donate-form-btn');
+const historyyButn = document.getElementById('history-form-btn');
+
+function activBton(btnToActiv){
+  donatttButn.classList.remove('active');
+  historyyButn.classList.remove('active');
+
+  btnToActiv.classList.add('active');
+}
+
+donatttButn.addEventListener('click', function(){
+   activBton(donatttButn);
+})
+
+historyyButn.addEventListener('click', function(){
+   activBton(historyyButn);
+})
